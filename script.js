d8a226d041299d061e9599d106fd0bfcd86baef0
Muskan <102637258+Fun-25@users.noreply.github.com>
2023-04-24 17:12:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(event) {
+	event.preventDefault(); // prevent form submission
 
+	let r = document.getElementById("radius");
+	let vol = document.getElementById("volume");
+	let rad = parseFloat(r.value);
+
+	if (isNaN(rad)) {
+		alert("Please enter a valid number for the radius.");
+		return;
+	}
+
+	let volume = (4 / 3) * Math.PI * Math.pow(rad, 3);
+	vol.value = volume.toFixed(2); // round to 2 decimal places
+}
+
+document.getElementById("calculate").addEventListener('click', volume_sphere);
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
